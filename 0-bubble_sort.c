c8405547267af5e3c7b787f0870353c095e83f23
yismaw17 <yismawmulaw@yahoo.com>
2023-01-25 13:04:33 +0000

    task algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,71 @@
 #include "sort.h"
 
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Return: void
+ * bubble_sort - Sort by swapping largest items to end of array
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
+
 void bubble_sort(int *array, size_t size)
-{
-size_t i, n, new_n;
-int swap;
 
-if (array == NULL || size < 2)
-return;
+{
 
-n = size;
-while (n > 0)
-{
-new_n = 0;
-for (i = 0; i < n - 1; i++)
-{
-if (array[i] > array[i + 1])
-{
-swap = array[i];
-array[i] = array[i + 1];
-array[i + 1] = swap;
-new_n = i + 1;
-print_array(array, size);
-}
-}
-n = new_n;
-}
+	size_t j;
+
+	int swapped;
+
+	size_t temp;
+
+	size_t tempsize;
+
+
+	if (array == NULL)
+
+		return;
+
+	tempsize = size;
+
+	swapped = 1;
+
+	while (swapped)
+
+	{
+
+		swapped = 0;
+
+		j = 0;
+
+		while (j < tempsize - 1)
+
+		{
+
+			if (array[j] > array[j + 1])
+
+			{
+
+				temp = array[j + 1];
+
+				array[j + 1] = array[j];
+
+				array[j] = temp;
+
+				print_array(array, size);
+
+				swapped = 1;
+
+			}
+
+			j++;
+
+		}
+
+		tempsize = j;
+
+	}
+
 }
